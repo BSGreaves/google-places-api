cb2e58d5da6f7049e6c96010fced138f65375342
Ben Greaves <bsgreaves@gmail.com>
2017-04-25 12:35:19 -0500

    Homework done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
-	$("body").on("click", "li", (e) =>{
-		console.log(e.target.innerHTML);
-	});
+const apiKey = "AIzaSyB7SFtRN28x-WefOlYZ6nfP0EZzOY7IOlo";
 
+$("body").on("click", "li", (e) => {
+	loadPlaces(e.target.innerHTML).then((data) => {
+		$("#output").html(data.results.map(result => {
+			return (`<h4>${result.name}</h4>`);
+		}));
+	}).catch((error) => {
+		console.log(error);
+	})
+});
 
-})
+	const loadPlaces = (dropdownType) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/
+				json?location=36.174465,-86.767960&radius=50000&type=${dropdownType}&key=${apiKey}
+				`)
+				.done((data) => resolve(data))
+				.fail((error) => reject(error));
+		});
+	};
+});
